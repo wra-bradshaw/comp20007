a17a30e03ba522cbc15c4d4312f0c1036cbb4891
will <will.bradshaw50@gmail.com>
2026-05-12 15:23:37 +1000

    feat: move scripts to justfile

--- flake.nix
+++ flake.nix
@@ -90,68 +90,6 @@
               ));
           };
 
-          generate-manifest = pkgs.writeShellApplication {
-            name = "generate-manifest";
-            runtimeInputs = [
-              pkgs.jq
-              pkgs.git
-            ];
-            text = ''
-              ROOT_DIRECTORY="$(git rev-parse --show-toplevel)"
-
-              find "$ROOT_DIRECTORY/projects" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | git check-ignore --stdin --verbose --non-matching | awk '$1 == "::" {print $2}' | jq -R -s 'split("\n")[:-1]' > "$ROOT_DIRECTORY/manifest.json"
-            '';
-          };
-
-          new-project = pkgs.writeShellApplication {
-            name = "new-project";
-            runtimeInputs = [
-              self.packages.${system}.generate-manifest
-              pkgs.git
-              pkgs.gnused
-            ];
-            text = ''
-              if [ -z "$1" ]; then
-              printf "Error: No directory name provided.\n" >&2
-              printf "Usage: %s <directory_name>\n" "$0" >&2
-              exit 1
-              fi
-
-              ROOT_DIRECTORY="$(git rev-parse --show-toplevel)/projects"
-              DIRECTORY="$ROOT_DIRECTORY/$1"
-
-              mkdir "$DIRECTORY"
-              cp --no-preserve=mode ${./boilerplate.c} "$DIRECTORY/main.c"
-              cp --no-preserve=mode ${./boilerplate_test.c} "$DIRECTORY/test.c"
-              sed "s/@PROJECT_NAME@/$1/g" ${./boilerplate_meson.build} > "$DIRECTORY/meson.build"
-              generate-manifest
-
-              git add "$DIRECTORY/main.c" "$DIRECTORY/test.c" "$DIRECTORY/meson.build"
-              git add "$(git rev-parse --show-toplevel)/manifest.json"
-            '';
-          };
-
-          debug = pkgs.writeShellApplication {
-            name = "debug";
-            runtimeInputs = [ pkgs.lldb ];
-            text = ''
-              PROJECT_NAME="$1"
-              if [ -z "$PROJECT_NAME" ]; then
-              	echo "Usage: $0 <project-name> [--test]" >&2
-              	exit 1
-              fi
-
-              if [ "''${2:-}" = "--test" ]; then
-                BINARY_PATH="$(nix build --print-out-paths --no-link ".#projects.debug.$PROJECT_NAME")/bin/unit-tests"
-                echo "$BINARY_PATH"
-                lldb "$BINARY_PATH"
-              else
-                BINARY_PATH="$(nix build --print-out-paths --no-link ".#projects.debug.$PROJECT_NAME")/bin/$PROJECT_NAME"
-                echo "$BINARY_PATH"
-                lldb "$BINARY_PATH"
-              fi
-            '';
-          };
         }
       );
 
@@ -167,9 +105,9 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               nodejs
-              self.packages.${system}.generate-manifest
-              self.packages.${system}.new-project
-              self.packages.${system}.debug
+              git
+              jq
+              lldb
               just
               meson
               ninja
--- justfile
+++ justfile
@@ -3,6 +3,13 @@ set shell := ["bash", "-eu", "-o", "pipefail", "-c"]
 default:
 	@just --list
 
+generate-manifest:
+	ROOT_DIRECTORY="$(git rev-parse --show-toplevel)"; \
+	find "$ROOT_DIRECTORY/projects" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | \
+		(git -C "$ROOT_DIRECTORY" check-ignore --stdin --verbose --non-matching || true) | \
+		awk '$1 == "::" {print $2}' | \
+		jq -R -s 'split("\n")[:-1]' > "$ROOT_DIRECTORY/manifest.json"
+
 setup project:
 	if [ -d "build/{{project}}" ]; then \
 		meson setup --reconfigure "build/{{project}}" "projects/{{project}}"; \
@@ -22,8 +29,25 @@ test project: (build project)
 debug project *args: (build project)
 	lldb "./build/{{project}}/{{project}}" -- {{args}}
 
+debug-nix project target="":
+	BINARY_NAME="{{project}}"; \
+	if [ "{{target}}" = "--test" ]; then \
+		BINARY_NAME="unit-tests"; \
+	fi; \
+	BINARY_PATH="$(nix build --print-out-paths --no-link ".#projects.debug.{{project}}")/bin/$BINARY_NAME"; \
+	printf '%s\n' "$BINARY_PATH"; \
+	lldb "$BINARY_PATH"
+
 clean project:
 	rm -rf "build/{{project}}"
 
 new project:
-	new-project "{{project}}"
+	ROOT_DIRECTORY="$(git rev-parse --show-toplevel)"; \
+	DIRECTORY="$ROOT_DIRECTORY/projects/{{project}}"; \
+	mkdir "$DIRECTORY"; \
+	install -m 0644 "$ROOT_DIRECTORY/boilerplate.c" "$DIRECTORY/main.c"; \
+	install -m 0644 "$ROOT_DIRECTORY/boilerplate_test.c" "$DIRECTORY/test.c"; \
+	sed "s/@PROJECT_NAME@/{{project}}/g" "$ROOT_DIRECTORY/boilerplate_meson.build" > "$DIRECTORY/meson.build"; \
+	just generate-manifest; \
+	git add "$DIRECTORY/main.c" "$DIRECTORY/test.c" "$DIRECTORY/meson.build"; \
+	git add "$ROOT_DIRECTORY/manifest.json"
